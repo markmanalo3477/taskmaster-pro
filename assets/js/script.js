829f016f1c691fb9f1dd68cb94edd67ab3bd0fa8
mark manalo <m.e.manalo77@gmail.com>
2023-05-23 00:07:12 -0400

    added ability to edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-   
+    console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -44,47 +44,6 @@ var loadTasks = function() {
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
-
-$(".list-group").on("click", "p", function() {
-  var text = $(this)
-  .text()
-  var textInput = $("<textarea>")
-  $(this).replaceWith(textInput)
-  textInput.trigger("focus")
-    .addClass("form-control")
-    .val(text);
-  
-});
-
-$(".list-group").on("blur", "textarea", function() {
-// get the textarea's current value/text
-var text= $(this)
-.val()
-.trim();
-//recreate p element
-var taskP = $("<p>")
-  .addClass("m-1")
-  .text(text);
-  //replace textarea with p element
-  $(this).replaceWith(taskP);
-//get the parent ul's id attribute
-var status = $(this)
-.closest(".list-group")
-.attr("id")
-.replace("list-","");
-});
-//get the task's position in the list of other li elements
-var index = $(this)
- .closest(".list-group-item")
- .index();
-
- tasks[status][index].text = text;
- saveTasks();
-
-
-
-
-
 
 
 
@@ -105,7 +64,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 $("#task-form-modal .btn-primary").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
-  var taskDate = $("#modalDueDate").val(); 
+  var taskDate = $("#modalDueDate").val();
 
   if (taskText && taskDate) {
     createTask(taskText, taskDate, "toDo");
@@ -120,9 +79,91 @@ $("#task-form-modal .btn-primary").click(function() {
     });
 
     saveTasks();
-    $(".list-group").on("click", "p", function() { console.log("<p> was clicked");
-});
   }
+});
+
+// task text was clicked
+$(".list-group").on("click", "p", function() {
+  // get current text of p element
+  var text = $(this)
+    .text()
+    .trim();
+
+  // replace p element with a new textarea
+  var textInput = $("<textarea>").addClass("form-control").val(text);
+  $(this).replaceWith(textInput);
+
+  // auto focus new element
+  textInput.trigger("focus");
+});
+
+// editable field was un-focused
+$(".list-group").on("blur", "textarea", function() {
+  // get current value of textarea
+  var text = $(this).val();
+
+  // get status type and position in the list
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].text = text;
+  saveTasks();
+
+  // recreate p element
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+
+  // replace textarea with new content
+  $(this).replaceWith(taskP);
+});
+
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+  $(this).replaceWith(dateInput);
+
+  // automatically bring up the calendar
+  dateInput.trigger("focus");
+});
+
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  var date = $(this).val();
+
+  // get status type and position in the list
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span and insert in place of input element
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+    $(this).replaceWith(taskSpan);
 });
 
 // remove all tasks
